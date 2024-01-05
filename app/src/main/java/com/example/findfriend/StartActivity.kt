8701b3214eae974e7cc1934192320f8cd8b4356c
tishakong <leehn45@naver.com>
2024-01-05 16:35:44 +0900

    기초 화면 재세팅, 로딩(스플래시) 화면 구현
    기초 화면 재세팅, 로딩(스플래시) 화면 구현

---
+++ app/src/main/java/com/example/findfriend/StartActivity.kt
@@ -0,0 +1,44 @@
+package com.example.findfriend
+
+import android.content.Intent
+import android.icu.text.CaseMap.Title
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+
+lateinit var fadeInAnim : Animation
+lateinit var appLogo : ImageView
+lateinit var appTitle: TextView
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        appTitle = findViewById(R.id.appTitle)
+        appLogo = findViewById(R.id.appLogo)
+        appLogo.startAnimation(fadeInAnim)
+        appTitle.startAnimation(fadeInAnim)
+
+        // 일정 시간 지연 이후 실행하기 위한 코드
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            // 일정 시간이 지나면 MainActivity로 이동
+            val intent= Intent( this,MainActivity::class.java)
+            startActivity(intent)
+
+            // 이전 키를 눌렀을 때 스플래스 스크린 화면으로 이동을 방지하기 위해
+            // 이동한 다음 사용안함으로 finish 처리
+            finish()
+
+        }, 1000) // 시간 1초 이후 실행
+
+    }
+}
