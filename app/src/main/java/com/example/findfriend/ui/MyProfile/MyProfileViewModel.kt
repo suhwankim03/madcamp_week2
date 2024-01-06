ce494e58c0a8e83dbbfceb6bb8a06139f411f92c
tishakong <leehn45@naver.com>
2024-01-06 13:16:11 +0900

    fragment 이름 수정 및 탭 아이콘 수정
    fragment 이름 수정 및 탭 아이콘 수정

--- app/src/main/java/com/example/findfriend/ui/MyProfile/MyProfileViewModel.kt
+++ app/src/main/java/com/example/findfriend/ui/MyProfile/MyProfileViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.findfriend.ui.home
+package com.example.findfriend.ui.MyProfile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MyProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is my profile Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/example/findfriend/ui/MyRoom/MyRoomFragment.kt
+++ app/src/main/java/com/example/findfriend/ui/MyRoom/MyRoomFragment.kt
@@ -1,4 +1,4 @@
-package com.example.findfriend.ui.home
+package com.example.findfriend.ui.MyRoom
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.findfriend.databinding.FragmentHomeBinding
+import com.example.findfriend.databinding.FragmentMyroomBinding
 
-class HomeFragment : Fragment() {
+class MyRoomFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentMyroomBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(MyRoomViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentMyroomBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
+        val textView: TextView = binding.myRoomText
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
