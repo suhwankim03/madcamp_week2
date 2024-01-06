84e60acd5e7ab419ebdff0de32d103f076263fcc
tishakong <leehn45@naver.com>
2024-01-06 16:34:40 +0900

    폰트 추가 및 리사이클뷰 의존성 추가
    폰트 추가 및 리사이클뷰 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.6.0")
     implementation("com.squareup.retrofit2:converter-gson:2.6.0")
     implementation("com.google.code.gson:gson:2.8.5")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
 }
